64f8a72b2e864b5625963bfbd9a8d98bb5757c79
Qasim <mqasimca@gmail.com>
2025-01-15 23:55:08 -0500

    update config

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ensure_installed pacman hyprland alacritty brightnessctl nwg-look hyprpaper waybar xsensors papirus-icon-theme
+ensure_installed pacman hyprland alacritty brightnessctl nwg-look hyprpaper waybar xsensors papirus-icon-theme waybar qt5ct notification-daemon
+ensure_installed pacman ttf-fantasque-nerd ttf-font-awesome otf-font-awesome awesome-terminal-fonts noto-fonts noto-fonts-emoji ttf-fira-sans ttf-hack cachyos-nord-gtk-theme-git capitaine-cursors kvantum-theme-nordic-git
